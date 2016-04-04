90dcf6d744254993d02f4e2fd9d8258bc34b605e
vincent <vincent030299@hotmail.com>
2016-04-04 14:47:38 +0200

    shop begin

--- public/cart.php
+++ public/cart.php
@@ -1,7 +1,6 @@
 <?php include'header.php';?>
 <?php include'database.php';?>
 
-
 <!-- below is html -->
 
 
--- public/shop.php
+++ public/shop.php
@@ -1,9 +1,23 @@
 <?php include'header.php';?>
 <?php include'database.php';?>
 
-
 <!-- below is html -->
+<div class="container">
+	<div class="row">
 
+		<?php
+
+		$query = "SELECT * FROM `cart_producten`";
+		// try to execute the query
+		$query_result = $conn->query( $query );
+		// returns the query
+		print_r($query_result->fetchAll(PDO::FETCH_ASSOC));
+
+
+		?>
+
+	</div>
+</div>
 
 <!-- above is html -->
 
